aa0f04ed9430dbe55291c0350801ca91d839f278
zhangqi61 <zhangqi61@baidu.com>
2021-11-27 16:12:30 +0800

    [update]

---
+++ app/src/main/java/com/dashingqi/dqtoast/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dashingqi.dqtoast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
